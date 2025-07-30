114bcbe65f443710cc3157a4e330256f6bd33539
Chinmay-395 <chinmaydali395@gmail.com>
2025-07-30 16:23:30 -0400

    updated the about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,22 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React',
+    'Python',
+    'Node.js',
+    'OCaml',
+    'Erlang',
+    'C++',
+    'LLVM',
+    'AWS(EC2,Lambda,S3)',
+    'Redis',
+    'Kafka',
+    'Docker',
+    'Kubernetes',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +150,21 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              With a master of Science in Computer Science from Stevens Institute of Technology, I’m
+              experienced in optimizing system performance, designing efficient APIs, and deploying
+              applications on cloud platforms. I enjoy breaking down complex problems and crafting
+              solutions that are both scalable and user-centric. Whether it's enhancing backend
+              efficiency or improving user interfaces, I focus on delivering clean, impactful code
+              that drives results. I’m excited about opportunities that allow me to push boundaries,
+              collaborate with innovative teams, and continuously learn while contributing to
+              meaningful projects.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="https://us.mullenlowe.com/">Corebridge Financials</a>, and{' '}
+              <a href="https://scout.camd.northeastern.edu/">LTI-Mindtree</a>. My main focus these
+              days is getting into Compiler research and going back to academia.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
